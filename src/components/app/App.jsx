75bc726a2e2ca0a9992b3b3414605212f79427f2
Mr-Desperado-19 <mr.arturhlebov@gmail.com>
2023-06-25 19:40:08 +0200

    Profile - bug

--- src/components/app/App.jsx
+++ src/components/app/App.jsx
@@ -3,12 +3,18 @@ import Statistics from "../statistics";
 import FriendList from "../friendList";
 import TransactionHistory from "../transactionHistory";
 
-
+import user from "/user.json";
 
 const App = () => {
   return (
     <div>
-        <Profile />
+        <Profile
+          username={user.username}
+          tag={user.tag}
+          location={user.location}
+          avatar={user.avatar}
+          stats={user.stats}
+        />
         <Statistics />
         <FriendList />
         <TransactionHistory />
